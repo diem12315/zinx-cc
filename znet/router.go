dd644f0f890fe3c8e95df15cc3e13e3c67f273c3
diem12315 <50873292+diem12315@users.noreply.github.com>
2021-04-27 20:47:10 +0800

    4.27 完成路由模块定义

--- znet/router.go
+++ znet/router.go
@@ -6,6 +6,10 @@ import "example.com/m/ziface"
 type BaseRouter struct {
 }
 
+// 这里之所以BaseRouter的方法都为空
+// 是因为有的Router不希望有PreHandle，PostHandle业务
+// 所有Router全部继承BaseRouter的好处，不需要实现PreHandle，PostHandle
+
 // 在处理conn业务之前的钩子方法Hook
 func (br *BaseRouter) PreHandle(request ziface.IRequeust) {
 
